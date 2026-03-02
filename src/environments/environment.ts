8b4d2e273c62609f437094459dd9b8aeb3a5ce96
angelhernandez <angel.lphernandez@gmail.com>
2026-03-02 08:52:21 -0800

    Added the correct environments.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,8 @@ export const environment = {
     production: false,
     name: 'dev',
     api: {
-        // url: 'http://localhost:3000/api',
-        url: 'http://192.168.1.29:3000/api',
+        url: 'http://localhost:3000/api',
+        // url: 'http://192.168.1.29:3000/api',
     }
 }
 // https://script.google.com/macros/s/AKfycbxb6MdacfSxFg50W1z9jFoRRviq2RoKgAnNWJfQoUDfz1oEwhBCvrd1jbJd7YU5IdHD/exec
--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: true,
     name: 'prod',
     api: {
-        url: 'https://script.google.com/macros/s/AKfycbzOBc9PdAmi6kbhr0Jk288b-evc9-E9s33MGflbNpZ7jS9Ml06b50v0oZRvxzMjF3c/exec',
+        url: 'https://lugoframing-backend.onrender.com/api',
     }
 }
